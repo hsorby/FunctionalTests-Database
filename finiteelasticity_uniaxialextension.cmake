16f70fdce98ba9ea9c24001d4bc4c41902839a94
Hugh Sorby <h.sorby@auckland.ac.nz>
2017-09-11 18:06:19 +1200

    Update finiteelasticity_uniaxialextension.cmake

--- finiteelasticity_uniaxialextension.cmake
+++ finiteelasticity_uniaxialextension.cmake
@@ -2,3 +2,4 @@
 set(TEST_URL https://github.com/hsorby/finiteelasticity_uniaxialextension.git)
 set(TEST_BRANCH master)
 set(TEST_TARGETS uniaxial_extension_f)
+set(TEST_EXPECTED_RESULTS src/fortran/expected_results)
