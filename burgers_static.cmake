3614fa330355f03f8d7461440a631195518f8986
Hugh Sorby <h.sorby@auckland.ac.nz>
2017-09-14 18:45:00 +1200

    Add burgers_static.cmake to new test.

---
+++ burgers_static.cmake
@@ -0,0 +1,6 @@
+
+set(TEST_URL https://github.com/OpenCMISS-Examples/burgers_static.git)
+set(TEST_BRANCH develop)
+set(TEST_TARGETS burgers_static_fortran)
+set(TEST_EXPECTED_RESULTS src/fortran/expected_results.txt)
+
