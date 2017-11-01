c0c3c53fa0aa4df55415d2486262129bf97c386c
Hugh Sorby <h.sorby@auckland.ac.nz>
2017-11-01 16:16:32 +1300

    Update format of bioelectrics to meet format standard.

--- bioelectrics_monodomain.cmake
+++ bioelectrics_monodomain.cmake
@@ -1,13 +1,15 @@
 
 set(TEST_URL https://github.com/hsorby/bioelectrics_monodomain.git)
 set(TEST_BRANCH master)
+
 set(TEST_TARGETS monodomain_2d_f)
+set(TEST_TARGETS_ARGS "0.001;0.1;1;src/python/n98.xml")
 set(TEST_EXPECTED_RESULTS src/fortran/expected_results)
 
 set(PYTEST_SCRIPTS src/python/Monodomain2DSquare.py)
-set(PYTEST_SCRIPT_1_ARGS src/python/n98.xml)
+set(PYTEST_SCRIPTS_ARGS "src/python/n98.xml")
 set(PYTEST_EXPECTED_RESULTS src/python/exptected_results)
 
-set(MPI TRUE)
-set(MPI_NP 4)
+set(TEST_MULTI_PROCESS TRUE)
+set(TEST_NP 4)
 
