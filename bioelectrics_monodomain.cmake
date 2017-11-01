ca0cbb9c67c05fc6a3e6562522766e070572505d
Hugh Sorby <h.sorby@auckland.ac.nz>
2017-11-02 12:12:44 +1300

    Change TEST_URL to TEST_GIT_REPO.

--- bioelectrics_monodomain.cmake
+++ bioelectrics_monodomain.cmake
@@ -1,15 +1,18 @@
 
-set(TEST_URL https://github.com/hsorby/bioelectrics_monodomain.git)
+set(TEST_GIT_REPO https://github.com/hsorby/bioelectrics_monodomain.git)
 set(TEST_BRANCH master)
 
 set(TEST_TARGETS monodomain_2d_f)
-set(TEST_TARGETS_ARGS "0.001;0.1;1;src/python/n98.xml")
+set(TEST_TARGETS_ARGS "0.001;0.1;1;src/fortran/n98.xml")
 set(TEST_EXPECTED_RESULTS src/fortran/expected_results)
-
-set(PYTEST_SCRIPTS src/python/Monodomain2DSquare.py)
-set(PYTEST_SCRIPTS_ARGS "src/python/n98.xml")
-set(PYTEST_EXPECTED_RESULTS src/python/exptected_results)
 
 set(TEST_MULTI_PROCESS TRUE)
 set(TEST_NP 4)
+
+set(PYTEST_TARGETS src/python/Monodomain2DSquare.py)
+set(PYTEST_TARGETS_ARGS "0.001;0.1;1;src/python/n98.xml")
+set(PYTEST_EXPECTED_RESULTS src/python/exptected_results)
+
+set(PYTEST_MULTI_PROCESS TRUE)
+set(PYTEST_NP 4)
 
--- finiteelasticity_uniaxialextension.cmake
+++ finiteelasticity_uniaxialextension.cmake
@@ -1,5 +1,5 @@
 
-set(TEST_URL https://github.com/hsorby/finiteelasticity_uniaxialextension.git)
+set(TEST_GIT_REPO https://github.com/hsorby/finiteelasticity_uniaxialextension.git)
 set(TEST_BRANCH master)
 set(TEST_TARGETS uniaxial_extension_f)
 set(TEST_EXPECTED_RESULTS src/fortran/expected_results)
